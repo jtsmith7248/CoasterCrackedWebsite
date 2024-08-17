4f2dcb3090499e08efd526368a97431aa1adb045
jtsmith7248 <97479064+jtsmith7248@users.noreply.github.com>
2024-08-17 19:21:42 -0400

    Add files via upload

---
+++ assets/js/footer.js
@@ -0,0 +1,3 @@
+document.write("<div style='text-align: center;'>");
+document.write("<a href='index.html'>Home</a> | <a href='rules.html'>Rules</a> | <a href='contenders.html'>Contenders</a> | <a href='vote.html'>Voting</a> | <a href='submit.html'>Submissions</a> | <a href='contact.html'>Contact Me!</a> | <a href='sitemap.xml'>Sitemap</a>");
+document.write("</div>");
